872f0082bcc536f6d171e0bb6ea825c6586b4fa1
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-06-17 18:03:34 +0200

    ©️ Add copyright notices

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">{children}</body>
+      <body className="bg-white text-gray-900 flex flex-col min-h-screen">
+        <div className="flex-grow">{children}</div>
+        <footer className="flex justify-between items-center text-sm text-gray-800 py-4 px-10">
+          <span>© {new Date().getFullYear()} WanderVisa</span>
+          <span>All rights reserved.</span>
+        </footer>
+      </body>
     </html>
   );
 }
