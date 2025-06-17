4bc6368baa7b0fac67f30924d7a6f93a0df41807
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-06-17 11:28:46 +0200

    ✨ Basic features

---
+++ lib/visaData.ts
@@ -0,0 +1,22 @@
+const visaData = [
+  {
+    passport_country: "France",
+    destination_country: "Thailand",
+    visa_requirement: "No Visa Required ✅",
+    notes: "Up to 30 days, extendable once by 30 more days.",
+  },
+  {
+    passport_country: "France",
+    destination_country: "Vietnam",
+    visa_requirement: "Visa Required ⚠️",
+    notes: "Apply online for e-Visa valid for 30 days.",
+  },
+  {
+    passport_country: "Germany",
+    destination_country: "Thailand",
+    visa_requirement: "No Visa Required ✅",
+    notes: "Up to 30 days with possible extension.",
+  },
+];
+
+export default visaData;
